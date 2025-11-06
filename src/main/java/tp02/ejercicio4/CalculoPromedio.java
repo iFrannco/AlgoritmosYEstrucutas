1870b1e9476f1f59b303f16aab72590f53ccc344
iFrannco <fcabeza91@gmail.com>
2025-11-06 12:06:46 -0300

    rehice el ejercicio: promedioMaximoPorNivel

--- src/main/java/tp02/ejercicio4/CalculoPromedio.java
+++ src/main/java/tp02/ejercicio4/CalculoPromedio.java
@@ -6,55 +6,69 @@ import tp01.ejercicio3.ColaGenerica;
 import tp02.estructuras.ArbolGeneral;
 
 import java.util.HashMap;
-import java.util.Map;
 
 public class CalculoPromedio {
 
-    private static int getMayorPromedio(Map<Integer, ListaGenerica<Integer>> mapa) {
+    public int promedioMaximoNivel(ArbolGeneral<AreaEmpresa> arbol) {
+        // recorrer por nivel
+        // anotar el nivel
+        // sumar el nivel a un mapa
+        // el mapa deberia tener el nivel y una lista de valores.
+        // recorrer el mapa para saber el promedio de cada nivel
+        // retornar el mayor promedio (guardar el primero e irlo comparando con el resto)
+
         int mayorPromedio = 0;
 
-        for (Map.Entry<Integer, ListaGenerica<Integer>> nivel : mapa.entrySet()) {
-            ListaGenerica<Integer> elementos = nivel.getValue();
-            int suma = 0;
-            int promedioNivel;
-            while (!elementos.fin()) {
-                suma += elementos.proximo();
+        if (!arbol.esVacio()) {
+            HashMap<Integer, ListaGenerica<Integer>> mapa = new HashMap<>();
+            ColaGenerica<ArbolGeneral<AreaEmpresa>> cola = new ColaGenerica<>();
+            ColaGenerica<Integer> niveles = new ColaGenerica<>();
+            cola.encolar(arbol);
+            niveles.encolar(0);
+
+            while (!cola.esVacia()) {
+                ArbolGeneral<AreaEmpresa> actual = cola.desencolar();
+                int nivelActual = niveles.desencolar();
+
+                // si el nivel no existe en el mapa, agregalo
+                if (!mapa.containsKey(nivelActual)) {
+                    mapa.put(nivelActual, new ListaEnlazadaGenerica<>());
+                }
+
+                // agregar el nodo actual en la lista correspondiente al nivel
+                ListaGenerica<Integer> valores = mapa.get(nivelActual);
+                valores.agregarFinal(actual.getDato().getTardanza_mensaje());
+
+                ListaGenerica<ArbolGeneral<AreaEmpresa>> hijos = actual.getHijos();
+                hijos.comenzar();
+                while (!hijos.fin()) {
+                    cola.encolar(hijos.proximo());
+                    niveles.encolar(nivelActual + 1);
+                }
+
             }
-            promedioNivel = suma / elementos.tamanio();
-            if (promedioNivel > mayorPromedio) {
-                mayorPromedio = promedioNivel;
+
+            // sumar los elementos de la lista, promediarlos y ver cual es el mayor.
+
+            // por cada nivel guardado en el mapa, tomar su lista
+            for (ListaGenerica<Integer> listaValores : mapa.values()) {
+                // definir un acumulador
+                int acumulador = 0;
+
+                // por cada elemento en la lista, sumarlo al contador
+                listaValores.comenzar();
+                while (!listaValores.fin()) {
+                    acumulador = +listaValores.proximo();
+                }
+
+                // si el promedio de este nivel es mayor al promedio encontrado, intercambiarlo
+                if (acumulador / listaValores.tamanio() > mayorPromedio) {
+                    mayorPromedio = acumulador / listaValores.tamanio();
+                }
             }
 
         }
+
         return mayorPromedio;
-    }
-
-    public int promedioMaximoNivel(ArbolGeneral<AreaEmpresa> arbol) {
-        ColaGenerica<ArbolGeneral<AreaEmpresa>> cola = new ColaGenerica<>();
-        ColaGenerica<Integer> niveles = new ColaGenerica<>();
-        cola.encolar(arbol);
-        niveles.encolar(0);
-
-        Map<Integer, ListaGenerica<Integer>> nodosPorNivel = new HashMap<>();
-
-
-        while (!cola.esVacia()) {
-            ArbolGeneral<AreaEmpresa> tope = cola.desencolar();
-            int nivelActual = niveles.desencolar();
-
-            if (!nodosPorNivel.containsKey(nivelActual)) {
-                nodosPorNivel.put(nivelActual, new ListaEnlazadaGenerica<>());
-            }
-            nodosPorNivel.get(nivelActual).agregarFinal(tope.getDato().getTardanza_mensaje());
-
-            ListaGenerica<ArbolGeneral<AreaEmpresa>> hijos = tope.getHijos();
-            while (!hijos.fin()) {
-                cola.encolar(hijos.proximo());
-                niveles.encolar(nivelActual + 1);
-            }
-            hijos.comenzar();
-        }
-
-        return getMayorPromedio(nodosPorNivel);
     }
 }
