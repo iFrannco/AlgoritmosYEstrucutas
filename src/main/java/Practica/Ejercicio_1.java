46ad766b654305b0c1459c23d311e1df9758db66
iFrannco <fcabeza91@gmail.com>
2025-11-06 23:58:35 -0300

    rehice el punto 1, ahora retorna el camino pedido cuando puede o uno alternativo si no hay uno que cumpla la condicion.

--- src/main/java/Practica/Ejercicio_1.java
+++ src/main/java/Practica/Ejercicio_1.java
@@ -7,71 +7,87 @@ import tp06.estructuras.Grafo;
 import tp06.estructuras.Vertice;
 
 public class Ejercicio_1 {
-    public ListaGenerica<String> buscarCamino(Grafo<String> ciudades, String origen, String destino, String
-            pasandoPor) {
 
-        ListaGenerica<Vertice<String>> listaVertices = ciudades.listaDeVertices();
-        listaVertices.comenzar();
-        Vertice<String> verticeorigen = null;
-        int encontrado = 0;
-        while (!listaVertices.fin() && encontrado != 2) {
-            Vertice<String> verticeActual = listaVertices.proximo();
-            if (verticeActual.dato().equals(origen)) {
-                encontrado++;
-                verticeorigen = verticeActual;
-            } else if (verticeActual.dato().equals(destino)) {
-                encontrado++;
+    public ListaGenerica<String> buscarCamino(Grafo<String> ciudades, String origen, String destino, String pasandoPor) {
+        if (!ciudades.esVacio()) {
+            // Verifico la existencia de la ciudad origen y destino
+            ListaGenerica<Vertice<String>> verticesCiudades = ciudades.listaDeVertices();
+            verticesCiudades.comenzar();
+            int ciudadesEncontradas = 0;
+            Vertice<String> verticeOrigen = null;
+            while (!verticesCiudades.fin() && !(ciudadesEncontradas == 2)) {
+                Vertice<String> v = verticesCiudades.proximo();
+                if (v.dato().equals(origen)) {
+                    verticeOrigen = v;
+                    ciudadesEncontradas++;
+                } else if (v.dato().equals(destino)) {
+                    ciudadesEncontradas++;
+                }
+            }
+
+            if (ciudadesEncontradas != 2) return new ListaEnlazadaGenerica<String>();
+
+            boolean[] visitados = new boolean[verticesCiudades.tamanio() + 1];
+            ListaGenerica<String> caminoPosible = new ListaEnlazadaGenerica<>();
+            ListaGenerica<ListaGenerica<String>> listaDeCaminos = new ListaEnlazadaGenerica<>();
+
+            dfs(ciudades, listaDeCaminos, caminoPosible, visitados, verticeOrigen, destino);
+
+            // teniendo los caminos posibles, si alguno cumple con la condicion de pasar por la ciudad de interes, lo retorno.
+            // si no, retorno el primero, y si no hay ningun camino retorno una lista vacia.
+
+            if (!listaDeCaminos.esVacia()) {
+                listaDeCaminos.comenzar();
+                while (!listaDeCaminos.fin()) {
+                    ListaGenerica<String> listaActual = listaDeCaminos.proximo();
+                    if (listaActual.incluye(pasandoPor)) {
+                        return listaActual;
+                    }
+                }
+                listaDeCaminos.comenzar();
+                return listaDeCaminos.proximo();
             }
         }
 
-        if (encontrado != 2) return new ListaEnlazadaGenerica<>();
-
-        ListaGenerica<String> camino = new ListaEnlazadaGenerica<>();
-        boolean[] enCamino = new boolean[listaVertices.tamanio() + 1];
-        boolean existeCamino = dfsRecursivo(ciudades, camino, verticeorigen, destino, enCamino, pasandoPor);
-
-
-        return existeCamino ? camino : new ListaEnlazadaGenerica<>();
+        return new ListaEnlazadaGenerica<>();
     }
 
-    private boolean dfsRecursivo(Grafo<String> grafo,
-                                 ListaGenerica<String> camino,
-                                 Vertice<String> nodoActual,
-                                 String destino, boolean[] enCamino,
-                                 String pasandoPor) {
+    private void dfs(Grafo<String> grafo, ListaGenerica<ListaGenerica<String>> listaDeCaminos,
+                     ListaGenerica<String> caminoPosible, boolean[] visitados, Vertice<String> vertice,
+                     String destino) {
 
-        // en lugar de tener una lista de visitados, si forma parte de los nodos del camino, lo evito.
-        if (enCamino[nodoActual.getPosicion()]) {
-            return false;
-        }
+        visitados[vertice.getPosicion()] = true;
+        caminoPosible.agregarFinal(vertice.dato());
 
-        enCamino[nodoActual.getPosicion()] = true;
-        camino.agregarFinal(nodoActual.dato());
-
-        if (nodoActual.dato().equals(destino)) {
-            if (camino.incluye(pasandoPor)) {
-                return true;
-            } else {
-                camino.eliminarEn(camino.tamanio());
-                enCamino[nodoActual.getPosicion()] = false;
-                return false;
+        if (vertice.dato().equals(destino)) {
+            listaDeCaminos.agregarFinal(copiar(caminoPosible));
+        } else {
+            ListaGenerica<Arista<String>> adyacentes = grafo.listaDeAdyacentes(vertice);
+            adyacentes.comenzar();
+            while (!adyacentes.fin()) {
+                Arista<String> arista = adyacentes.proximo();
+                Vertice<String> v = arista.verticeDestino();
+                if (!visitados[v.getPosicion()]) {
+                    dfs(grafo, listaDeCaminos, caminoPosible, visitados, v, destino);
+                }
             }
         }
 
-        ListaGenerica<Arista<String>> adyacentes = grafo.listaDeAdyacentes(nodoActual);
-        adyacentes.comenzar();
-        while (!adyacentes.fin()) {
-            Arista<String> arista = adyacentes.proximo();
-            Vertice<String> verticeTemp = arista.verticeDestino();
-            if (dfsRecursivo(grafo, camino, verticeTemp, destino, enCamino, pasandoPor)) {
-                return true;
-            }
+        // ya sea porque encontro un camino valido o porque llego al final de los adyacentes, se quita el nodo actual
+        // y se vuelve en la recursion para buscar otro camino.
+        caminoPosible.eliminarEn(caminoPosible.tamanio());
+        visitados[vertice.getPosicion()] = false;
 
-        }
-        // quitar de la lista porque recorri sus adyacentes y no es el camino correcto.
-        // ademas lo marco como que no forma parte del camino, para usarlo quizas en otro camino distino
-        camino.eliminarEn(camino.tamanio());
-        enCamino[nodoActual.getPosicion()] = false;
-        return false;
     }
+
+    private ListaGenerica<String> copiar(ListaGenerica<String> original) {
+        ListaGenerica<String> copia = new ListaEnlazadaGenerica<>();
+        original.comenzar();
+        while (!original.fin()) {
+            copia.agregarFinal(original.proximo());
+        }
+
+        return copia;
+    }
+
 }
