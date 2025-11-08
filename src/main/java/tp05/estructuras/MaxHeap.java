390960b8ca74af708adf3e40b268b64ea5621fe8
iFrannco <fcabeza91@gmail.com>
2025-11-07 21:13:21 -0300

    Le añadi comentarios a los algoritmos de MaxHeap

--- src/main/java/tp05/estructuras/MaxHeap.java
+++ src/main/java/tp05/estructuras/MaxHeap.java
@@ -36,6 +36,7 @@ public class MaxHeap<T extends Comparable<T>> implements ColaPrioridad<T> {
     public boolean agregar(T elemento) {
         // contempla que exista espacio
         try {
+            // añado el primer elemento en la pos 1 del array. en la 0 no hay nada.
             this.datos[++this.cantElementos] = elemento;
             perlocate_up();
         } catch (Exception e) {
@@ -46,12 +47,16 @@ public class MaxHeap<T extends Comparable<T>> implements ColaPrioridad<T> {
 
     private void perlocate_up() {
         int indice_hijo = cantElementos;
-        int indice_padre = indice_hijo / 2;
+        int indice_padre = indice_hijo / 2; // se queda con la parte entera de la division
 
+        // mientras el elemento analizado no sea la raiz (porque esta no tiene padre)
+        // y mientras ese elemento sea mayor al padre, realizar el intercambio
         while (indice_hijo > 1 && datos[indice_hijo].compareTo(datos[indice_padre]) > 0) {
             T temp = datos[indice_padre];
             datos[indice_padre] = datos[indice_hijo];
             datos[indice_hijo] = temp;
+            // actualizo los indices para seguir subiendo y comprobar que la propiedad de
+            // heap se cumple.
             indice_hijo = indice_padre;
             indice_padre = indice_hijo / 2;
         }
@@ -65,31 +70,47 @@ public class MaxHeap<T extends Comparable<T>> implements ColaPrioridad<T> {
         boolean cumple = false;
 
         while (!cumple) {
+            // compruebo si tengo hijo izquierdo, para hacer algo en caso de que la propiedad de heap no se cumpla
             if (cantElementos >= indice_hijo_izq) {
+                // compruebo si tengo hijo derecho, para hacer algo si la propiedad de heap no se cumple.
                 if (cantElementos >= indice_hijo_der) {
-                    if ((datos[indice_hijo_izq].compareTo(datos[indice_hijo_der]) > 0) && datos[indice_hijo_izq].compareTo(datos[indice_raiz]) > 0) {
+                    // si el hijo izquierdo es mayor al derecho y ademas el izquierdo es tambien mayor a la raiz,
+                    // intercambio el hijo izquierdo por la raiz
+                    if ((datos[indice_hijo_izq].compareTo(datos[indice_hijo_der]) > 0) &&
+                            datos[indice_hijo_izq].compareTo(datos[indice_raiz]) > 0) {
                         T temp = this.datos[indice_raiz];
                         this.datos[indice_raiz] = this.datos[indice_hijo_izq];
                         this.datos[indice_hijo_izq] = temp;
                         indice_raiz = indice_hijo_izq;
-                    } else if ((datos[indice_hijo_der].compareTo(datos[indice_hijo_izq]) > 0) && datos[indice_hijo_der].compareTo(datos[indice_raiz]) > 0) {
+                    } // si el hijo derecho es mayor al izquierdo y ademas es mayor a la raiz,
+                    // intercambio el hijo izquierdo por la raiz
+                    else if ((datos[indice_hijo_der].compareTo(datos[indice_hijo_izq]) > 0) &&
+                            datos[indice_hijo_der].compareTo(datos[indice_raiz]) > 0) {
                         T temp = this.datos[indice_raiz];
                         this.datos[indice_raiz] = this.datos[indice_hijo_der];
                         this.datos[indice_hijo_der] = temp;
                         indice_raiz = indice_hijo_der;
-                    } else {
+                    }
+                    // si ninguna ninguno de los hijos es mayor a la raiz, no hago nada.
+                    else {
                         cumple = true;
                     }
-                } else if (datos[indice_hijo_izq].compareTo(datos[indice_raiz]) > 0) {
+                }
+                // compruebo si el hijo izquierdo es mayor a la raiz, para intercambiarlos
+                else if (datos[indice_hijo_izq].compareTo(datos[indice_raiz]) > 0) {
                     T temp = this.datos[indice_raiz];
                     this.datos[indice_raiz] = this.datos[indice_hijo_izq];
                     this.datos[indice_hijo_izq] = temp;
                     indice_raiz = indice_hijo_izq;
-                } else {
+                }
+                // si tengo hijo izquierdo pero no es mayor a la raiz, no hago nada.
+                else {
                     cumple = true;
                 }
 
-            } else {
+            }
+            // si no tengo hijo izquierdo (y tampoco derecho, porque es un arbol completo) no es necesario hacer nada.
+            else {
                 cumple = true;
             }
             indice_hijo_izq = indice_raiz * 2;
